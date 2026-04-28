183ee656995d5a4b4d9f04e01782cdd7549e0ddd
swings27 <anna.trbd@gmail.com>
2026-04-28 16:01:36 +0200

    fix/fixed some bugs on artisan table

--- database/01_create.sql
+++ database/01_create.sql
@@ -37,9 +37,10 @@ CREATE TABLE artisan (
     site_web VARCHAR(255) NULL,
     a_propos VARCHAR(500) NOT NULL,
     top BOOLEAN DEFAULT FALSE,
+    id_specialite INT NOT NULL,
     CONSTRAINT pk_artisan PRIMARY KEY (id_artisan),
     CONSTRAINT uq_email UNIQUE (email),
-    CONSTRAINT chk_note CHECK (note >= 1 AND note =< 5),
+    CONSTRAINT chk_note CHECK (note >= 0 AND note <= 5),
     CONSTRAINT fk_appartient_specialite FOREIGN KEY (id_specialite) REFERENCES specialite(id_specialite)
     ON UPDATE CASCADE ON DELETE RESTRICT
 );
