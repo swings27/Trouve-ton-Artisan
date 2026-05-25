10f84e75aaf4608a611c81537a68b3e1f7b97227
swings27 <anna.trbd@gmail.com>
2026-05-25 19:36:10 +0200

    build/pages home and listing done

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import image from "../assets/images/erreur-404.png"
+import image from "../assets/images/erreur-404.png";
+import Container from 'react-bootstrap/Container';
 
 export default function NotFound() {
     return (
-        <>
+            <Container fluid="lg">
             <img src={image} alt="" aria-hidden="true" />
             <h1>Page non trouvée.</h1>
-        </>
+            </Container>
     )
 };
