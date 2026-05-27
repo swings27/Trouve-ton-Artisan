94102c1640dc3273eab5b602b1514c143a9122c7
swings27 <anna.trbd@gmail.com>
2026-05-27 16:10:44 +0200

    build/notfound and legals pages done

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import image from "../assets/images/erreur-404.png";
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 
 export default function NotFound() {
-    return (
-            <Container fluid="lg">
-            <img src={image} alt="" aria-hidden="true" />
-            <h1>Page non trouvée.</h1>
-            </Container>
-    )
-};
+	return (
+		<Container fluid="lg" className="mt-5">
+			<div className="d-flex flex-column align-items-center gap-4">
+				<img src={image} alt="" aria-hidden="true" className="img-fluid" />
+				<h1>Page non trouvée.</h1>
+			</div>
+		</Container>
+	);
+}
