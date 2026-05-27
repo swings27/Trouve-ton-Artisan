2e582520bf12d5f7a879afaadd858d7dc55480bb
swings27 <anna.trbd@gmail.com>
2026-05-27 16:04:19 +0200

    build/artisan page done

--- client/src/pages/Artisan.js
+++ client/src/pages/Artisan.js
@@ -32,32 +32,32 @@ export default function Artisan() {
 
 			{!loading && !error && artisan && (
 				<div className="mt-5 mx-5">
-					<section className="d-flex flex-row justify-content-between align-items-end">
+					<section className="artisan-header d-flex flex-row justify-content-between align-items-end">
 						<div>
 							<hr className="top-title" />
-							<h2>{artisan.nom}</h2>
+							<h1>{artisan.nom}</h1>
 							<h3>{artisan.Specialite.nom}</h3>
 						</div>
-						<h4>{artisan.site_web}</h4>
+						<a href={artisan.site_web}>Site web de {artisan.nom}</a>
 					</section>
-					<section className="mx-3 mt-5">
-						<Row>
-							<Col xs={12} md={5}>
+					<section className="mx-3 my-5">
+						<Row className="gx-5">
+							<Col xs={12} md={6} className="artisan-infos mt-3 mb-5">
 								<img
 									src={picdefault}
 									alt="Menuisier travaillant le bois"
 									className="defaultpic img-fluid"
 								/>
 								<div className="artisan-bar d-flex flex-row justify-content-around align-items-center my-4 p-1 rounded-pill">
-									{artisan.ville}
+									<div className="fw-bold">{artisan.ville}</div>
 									<StarsNote note={artisan.note} />
 								</div>
-								<div className="mx-3 p-2 border-start">
+								<div className="mx-3 p-2 h-25 border-start">
 									<h5>A PROPOS</h5>
 									<p>{artisan.a_propos}</p>
 								</div>
 							</Col>
-							<Col xs={12} md={{span: 5, offset: 2}} className="form-section">
+							<Col xs={12} md={6} className="form-section">
 								<hr className="top-title contact" />
 								<h3>Contacter</h3>
 								<FormArtisan id={id} emailArtisan={artisan.email} />
